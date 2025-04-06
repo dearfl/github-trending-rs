2feabe8f41aec0085d9986e7ec9161720ba591b0
dearfl <i@flr.me>
2025-04-06 08:03:37 +0800

    update archive branch

--- examples/rust-daily.rs
+++ examples/rust-daily.rs
@@ -1,9 +1,23 @@
-use std::time::Duration;
+use std::{
+    collections::HashSet,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
 
 use github_trending_rs::{Client, Language, Since};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let history: HashSet<String> = std::fs::read_dir("./archive")?
+        .flat_map(|dir| {
+            dir.ok().and_then(|dir| {
+                std::fs::File::open(dir.path())
+                    .ok()
+                    .map(|f| std::io::BufReader::new(f))
+                    .and_then(|reader| serde_json::from_reader(reader).ok())
+            })
+        })
+        .flat_map(|data: Vec<String>| data.into_iter())
+        .collect();
     let client = reqwest::ClientBuilder::new()
         .connect_timeout(Duration::from_secs(5))
         .timeout(Duration::from_secs(10))
@@ -15,8 +29,46 @@ async fn main() -> anyhow::Result<()> {
         .since(Since::Daily)
         .repositories()
         .await?;
+    let escape = |out: &str| {
+        let out = out.replace("-", "\\-");
+        let out = out.replace("+", "\\+");
+        let out = out.replace("_", "\\_");
+        let out = out.replace("*", "\\*");
+        let out = out.replace("~", "\\~");
+        let out = out.replace("#", "\\#");
+        let out = out.replace("=", "\\=");
+        let out = out.replace(".", "\\.");
+        let out = out.replace("!", "\\!");
+        let out = out.replace("|", "\\|");
+        let out = out.replace("(", "\\(");
+        let out = out.replace(")", "\\)");
+        let out = out.replace("[", "\\[");
+        let out = out.replace("]", "\\]");
+        let out = out.replace("{", "\\{");
+        let out = out.replace("}", "\\}");
+        let out = out.replace("`", "\\`");
+        let out = out.replace(">", "\\>");
+        out
+    };
     for repo in trending.iter() {
-        println!("[{}]({}): {}", repo.name, repo.url(), repo.description);
+        let ident = format!("{}/{}", repo.owner, repo.name);
+        if history.contains(&ident) {
+            continue;
+        }
+        println!(
+            "[{}]({}): {}",
+            escape(&repo.name),
+            escape(&repo.url()),
+            escape(&repo.description)
+        );
     }
+    let repos: Vec<_> = trending
+        .iter()
+        .map(|repo| format!("{}/{}", repo.owner, repo.name))
+        .collect();
+    let content = serde_json::to_string(&repos)?;
+    let now = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
+    let path = format!("./archive/{}.json", now);
+    std::fs::write(path, content)?;
     Ok(())
 }
